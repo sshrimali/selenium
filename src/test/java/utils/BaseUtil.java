a0ad6cf913ab5457f9b7e10fc4901981de0f39cc
Sourabh Shrimali <sourabh_shrimali@persistent.co.in>
2016-02-25 21:31:31 -0800

    updated code

--- src/test/java/utils/BaseUtil.java
+++ src/test/java/utils/BaseUtil.java
@@ -1,11 +1,21 @@
 package utils;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by sourabh on 2/25/16.
  */
 public class BaseUtil {
     private String browser = "firefox";
+    private DesiredCapabilities capabilities;
+    private WebDriver driver;
 
     public BaseUtil() throws Exception
     {
@@ -20,26 +30,35 @@ public class BaseUtil {
 
     public void setup()
  {
-     getWebDriver();
+
+     driver = getWebDriver();
+     driver.get("http://www.google.com/");
+     driver.close();
  }
 
-    private String getWebDriver() {
+    private WebDriver getWebDriver() {
         try
         {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+
             if (browser.equalsIgnoreCase("firefox"))
             {
-//                FirefoxProfile profile = new FirefoxProfile();
-//                // add any custom firefox configurations...
-//                profile.setPreference("javascript.options.showInConsole", true);
-//                profile.setPreference("dom.max_script_run_time", 340);
-//                profile.setPreference("dom.max_chrome_script_run_time", 460);
-//                profile.setEnableNativeEvents(true);
-//                capabilities.setCapability(FirefoxDriver.PROFILE,profile);
+                capabilities.setBrowserName("firefox");
+                FirefoxProfile profile = new FirefoxProfile();
+                // add any custom firefox configurations...
+                profile.setPreference("javascript.options.showInConsole", true);
+                profile.setPreference("dom.max_script_run_time", 340);
+                profile.setPreference("dom.max_chrome_script_run_time", 460);
+                profile.setEnableNativeEvents(true);
+                capabilities.setCapability(FirefoxDriver.PROFILE,profile);
             }
-    //        if (driver == null)
-    //        {
-    //           WebDriver driver = new FirefoxDriver(capabilities);
-    //        }
+            capabilities.setJavascriptEnabled(true);
+            capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            if (driver == null)
+            {
+               driver = new FirefoxDriver(capabilities);
+            }
 
         }
         catch (Exception e)
@@ -47,8 +66,8 @@ public class BaseUtil {
             e.printStackTrace();
         }
 
-        String webDriver="";
-        return webDriver;
+
+        return driver;
 
     }
 
